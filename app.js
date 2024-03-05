61557c7fb2878803de1e4d247078a6d3eb81a69a
anish102 <anishkarki695@gmail.com>
2024-03-05 21:20:57 +0545

    Routing

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+const routes = require(`./routes`);
+
+app.use(`/api`, routes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
