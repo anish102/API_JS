c3598b01e0c5b4c705b8a56498778d75d1b8a54c
anish102 <anishkarki695@gmail.com>
2024-04-02 21:56:42 +0545

    Api error handling / utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).reject((err) => next(err));
+  };
+};
+
+export default asyncHandler;
+
+// const asyncHandler = (fn) => {
+//     async () => {
+//       try {
+//         await fn(req, res, next);
+//       } catch (error) {
+//         res
+//           .status(err.code || 500)
+//           .json({ succeess: false, meassage: err.meassage });
+//       }
+//     };
+//   };
